9052b6f29639abbc0884e80d4ae77590f50b0527
PS750825 <szymczyk.pawel@yahoo.co.uk>
2017-04-29 16:07:23 +0100

    Interface

---
+++ PingPong/src/pingpong/InitValues.java
@@ -0,0 +1,34 @@
+package pingpong;
+
+/**
+ * This is interface with all initial values. 
+ * 
+ * @author Pawel Szymczyk
+ */
+public interface InitValues {
+    
+    public static final int FRAME_WIDTH = 1000;
+    public static final int FRAME_HEIGHT = 900;
+    
+    public static final int BOARD_WIDTH = 500;
+    public static final int BOARD_HEIGHT = 800;
+    
+    public static final int BALL_INITIAL_POS_X = 250;
+    public static final int BALL_INITIAL_POS_Y = 400;
+    
+    public static final int BALL_RADIUS = 5;
+    
+    public static final int PADDLE_PLAYER_WIDTH = 30;
+    public static final int PADDLE_PLAYER_HEIGHT = 10;
+    public static final int PADDLE_PLAYER_INITIAL_POS_X = 250;
+    public static final int PADDLE_PLAYER_INITIAL_PoS_Y = 750;
+    
+    public static final int PADDLE_AI_WIDTH = 30;
+    public static final int PADDLE_AI_HEIGHT = 10;
+    public static final int PADDLE_AI_INITIAL_POS_X = 250;
+    public static final int PADDLE_AI_INITIAL_POS_Y = 50;
+    
+    public static final int REDLINE_POS_Y = 400;
+    
+    
+}
--- PingPong/src/pingpong/PingPong.java
+++ PingPong/src/pingpong/PingPong.java
@@ -1,21 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pingpong;
 
 /**
- *
+ * This is simple Ping Pong Game. 
+ * This game is between Player and AI Player.
+ * 
  * @author Pawel Szymczyk
  */
 public class PingPong {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
