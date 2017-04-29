95584f1e362c7e4fee5ccbdde520eaff8fc92891
PS750825 <szymczyk.pawel@yahoo.co.uk>
2017-04-29 16:37:03 +0100

    screen size changes

--- PingPong/src/pingpong/InitValues.java
+++ PingPong/src/pingpong/InitValues.java
@@ -8,10 +8,10 @@ package pingpong;
 public interface InitValues {
     
     public static final int FRAME_WIDTH = 1000;
-    public static final int FRAME_HEIGHT = 900;
+    public static final int FRAME_HEIGHT = 800;
     
     public static final int BOARD_WIDTH = 500;
-    public static final int BOARD_HEIGHT = 800;
+    public static final int BOARD_HEIGHT = 700;
     
     public static final int BALL_INITIAL_POS_X = 250;
     public static final int BALL_INITIAL_POS_Y = 400;
@@ -21,14 +21,14 @@ public interface InitValues {
     public static final int PADDLE_PLAYER_WIDTH = 30;
     public static final int PADDLE_PLAYER_HEIGHT = 10;
     public static final int PADDLE_PLAYER_INITIAL_POS_X = 250;
-    public static final int PADDLE_PLAYER_INITIAL_PoS_Y = 750;
+    public static final int PADDLE_PLAYER_INITIAL_PoS_Y = 650;
     
     public static final int PADDLE_AI_WIDTH = 30;
     public static final int PADDLE_AI_HEIGHT = 10;
     public static final int PADDLE_AI_INITIAL_POS_X = 250;
     public static final int PADDLE_AI_INITIAL_POS_Y = 50;
     
-    public static final int REDLINE_POS_Y = 400;
+    public static final int REDLINE_POS_Y = 350;
     
     
 }
--- PingPong/src/pingpong/PingPong.java
+++ PingPong/src/pingpong/PingPong.java
@@ -1,16 +1,63 @@
 package pingpong;
 
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+
 /**
  * This is simple Ping Pong Game. 
  * This game is between Player and AI Player.
  * 
  * @author Pawel Szymczyk
  */
-public class PingPong {
-
+public class PingPong extends JFrame {
     
+    /**
+     * This constructor creates frame, add panel called Board and add Window Listeners.
+     */
+    public PingPong() {
+        
+        /* These parameters sets frame on the center of screen. */
+        int screenWidth = (Toolkit.getDefaultToolkit().getScreenSize().width  - getSize().width) / 4;
+        int screenHeight = (Toolkit.getDefaultToolkit().getScreenSize().height - getSize().height) / 12;
+        
+        /* Frame parameters */
+        setTitle("Ping Pong");
+        setSize(InitValues.FRAME_WIDTH, InitValues.FRAME_HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocation(screenWidth, screenHeight);
+        setResizable(false);
+        setVisible(true);
+        
+        /* Window Listeners */
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        
+        /* Add Panel */
+        
+        //Container panelContainer = getContentPane();
+        //panelContainer.add(new Board());
+    }
+    
+    /**
+     * This is main method, which run game.
+     * @param args nothing.
+     */
     public static void main(String[] args) {
         
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+		JFrame frame = new PingPong();
+               // frame.show();
+            }
+        });    
     }
     
 }
